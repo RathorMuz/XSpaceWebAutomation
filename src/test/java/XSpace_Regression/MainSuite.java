24fcda61119a29b73adfcac5382e67d18d19c2c9
muzammilqamar <mohammad.muzzamil@daraz.pk>
2023-12-01 19:33:01 +0500

    test

--- src/test/java/XSpace_Regression/MainSuite.java
+++ src/test/java/XSpace_Regression/MainSuite.java
@@ -52,7 +52,7 @@ public class MainSuite {
         occ.BuyerSelection(driver);
     }
 
-    @Test(priority = 3, retryAnalyzer = Retry.class)
+    @Test(priority = 3)
     public void SelectBuyer() throws InterruptedException {
         OpenCustomerCenter occ = new OpenCustomerCenter(driver);
         occ.SelectBuyer(driver);
@@ -68,32 +68,32 @@ public class MainSuite {
       OpenCustomerCenter occ = new OpenCustomerCenter(driver);
     occ.WalletInfo(driver);
     }
-    @Test(priority = 5,retryAnalyzer = Retry.class)
+    @Test(priority = 5)
     public void SearchbyPhoneNumber () throws InterruptedException {
         OpenCustomerCenter occ = new OpenCustomerCenter(driver);
         occ.SearchbyPhoneNumber(driver);
     }
-    @Test(priority = 6,retryAnalyzer = Retry.class)
+    @Test(priority = 6)
     public void SearchbyUserID () throws InterruptedException {
         OpenCustomerCenter occ = new OpenCustomerCenter(driver);
         occ.SearchbyUserID(driver);
     }
-    @Test(priority = 7,retryAnalyzer = Retry.class)
+    @Test(priority = 7)
     public void SearchbyOrderID () throws InterruptedException {
         OpenCustomerCenter occ = new OpenCustomerCenter(driver);
         occ.SearchbyOrderID(driver);
     }
-    @Test(priority = 8,retryAnalyzer = Retry.class)
+    @Test(priority = 8)
     public void SearchbyItemID () throws InterruptedException {
         OpenCustomerCenter occ = new OpenCustomerCenter(driver);
         occ.SearchbyItemID(driver);
     }
-    @Test(priority = 9,retryAnalyzer = Retry.class)
+    @Test(priority = 9)
     public void SearchSOP () throws InterruptedException {
         OpenCustomerCenter occ = new OpenCustomerCenter(driver);
         occ.SearchSOP(driver);
     }
-    @Test(priority = 10,retryAnalyzer = Retry.class)
+    @Test(priority = 10)
     public void SearchVoucher () throws InterruptedException {
         OpenCustomerCenter occ = new OpenCustomerCenter(driver);
         occ.SearchVoucher(driver);
@@ -103,12 +103,12 @@ public class MainSuite {
         CaseCenter cc = new CaseCenter(driver);
         cc.clickCaseCenterIcon(driver);
     }
-    @Test(priority = 12,retryAnalyzer = Retry.class)
+    @Test(priority = 12)
     public void C360DarazPluginData () {
         C360BuyerPlugin bp = new C360BuyerPlugin(driver);
         bp.C360DarazPluginData(driver);
     }
-    @Test(priority = 13,retryAnalyzer = Retry.class)
+    @Test(priority = 13)
     public void CreateCase () throws InterruptedException {
         CaseCenter cs = new CaseCenter(driver);
         cs.CreateCase(driver);
